06b62fd0467a36af2c5385722e3a21efb8f632b4
Amir M <amir.m@allshoreresources.com>
2012-12-29 02:04:28 +0500

    Js from code academy

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 'var val = number / 3;\n'+
                 'console.log(val);\n'+
                 '};\n'+
-                'divideByThree(6);');
+                'divideByThree(6);\n');
             var divideByThree = function (number) {
                 var val = number / 3;
                 console.log(val);
@@ -31,11 +31,11 @@
             
             console.log('var timesTwo = function(number) {\n'+
                 'return number * 2;\n'+
-            '};\n'+
-            ' \n'+
-            '// call timesTwo here\n'+
-            'var newNumber = timesTwo(10);\n'+
-            'console.log(newNumber);');
+                '};\n'+
+                ' \n'+
+                '// call timesTwo here\n'+
+                'var newNumber = timesTwo(10);\n'+
+                'console.log(newNumber);\n');
             
             var timesTwo = function(number) {
                 return number * 2;
@@ -45,6 +45,89 @@
             var newNumber = timesTwo(10);
             console.log(newNumber);
 
+            break;
+        
+        case '5':
+            
+            console.log('var multiplied;\n'+
+                'var timesTwo = function(number) {\n'+
+                'multiplied = number * 2;\n'+
+                '};\n'+
+                '\n'+
+                'timesTwo(4);\n'+
+                'console.log(multiplied);\n');
+            
+            var multiplied;
+            var timesTwo = function(number) {
+                multiplied = number * 2;
+            };
+
+            timesTwo(4);
+            console.log(multiplied);
+            
+            break;
+        
+        case '6':
+            
+            console.log('var junkData = ["Eddie Murphy", 49, "peanuts", 31];\n'+
+                '\n'+
+                'console.log(junkData[3])\n');
+            
+            var junkData = ["Eddie Murphy", 49, "peanuts", 31];
+            console.log(junkData[3])
+            
+            break;
+     
+        case '7':
+            
+            console.log('var cities = ["Melbourne", "Amman", "Helsinki", "NYC","Melbourne", "Amman", "Helsinki", "NYC"];\n'+
+                '\n'+
+                'for (var i = 0; i < cities.length; i++) {\n'+
+                'console.log("I would like to visit" + " " + cities[i]);\n'+
+                '}\n');
+            
+            var cities = ["Melbourne", "Amman", "Helsinki", "NYC","Melbourne", "Amman", "Helsinki", "NYC"];
+
+            for (var i = 0; i < cities.length; i++) {
+                console.log("I would like to visit" + " " + cities[i]);
+            }
+            
+            break;
+            
+        case '8':
+            
+            console.log('var coin = Math.floor(Math.random() * 2);\n'+
+                '\n'+
+                'while(coin){\n'+
+                'console.log("Heads! Flipping again...");\n'+
+                'var coin = Math.floor(Math.random() * 2);\n'+
+                '}\n'+
+                'console.log("Tails! Done flipping.");\n');
+            
+            var coin = Math.floor(Math.random() * 2);
+
+            while(coin){
+                console.log("Heads! Flipping again...");
+                var coin = Math.floor(Math.random() * 2);
+            }
+            console.log("Tails! Done flipping.");
+            
+            break;
+            
+            console.log('console.log(isNaN("berry"));\n'+
+            'console.log(isNaN(NaN));\n'+
+            'console.log(isNaN(undefined));\n'+
+            'console.log(isNaN(42));\n');
+            
+            console.log(isNaN("berry"));
+            console.log(isNaN(NaN));
+            console.log(isNaN(undefined));
+            console.log(isNaN(42));
+            
+        case '9':
+            
+            
+            
             break;
             
     }
